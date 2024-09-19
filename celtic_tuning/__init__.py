b20a5b08818e3379396c2f0281f29f205d98ba57
tigattack <10629864+tigattack@users.noreply.github.com>
2024-09-19 01:54:32 +0100

    expose `resolve_unit_case` func

--- celtic_tuning/__init__.py
+++ celtic_tuning/__init__.py
@@ -3,12 +3,14 @@
 from .celtic import Celtic
 from .enums import PowerUnits, TorqueUnits
 from .models import CelticData, PowerDetail, VehicleDetail
+from .utils import resolve_unit_case
 
 __all__ = [
     "Celtic",
     "CelticData",
     "PowerDetail",
     "PowerUnits",
+    "resolve_unit_case",
     "TorqueUnits",
     "VehicleDetail",
 ]
